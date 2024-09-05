9552f1881ff10569e916d9387f53620d64de392e
Joel Iziren <izirenjoel@gmail.com>
2024-09-05 01:46:04 +0100

    Embedding expressions, functions

---
+++ 0x01-react_intro/task_1/dashboard/src/utils.js
@@ -0,0 +1,9 @@
+// Function to get the current year
+export function getFullYear() {
+  return new Date().getFullYear();
+}
+
+// Function to get the footer copy based on the isIndex argument
+export function getFooterCopy(isIndex) {
+  return isIndex ? 'Holberton School' : 'Holberton School main dashboard';
+}
