871fab420d3abfc5e386c7ddc56d006e5321c07e
Joel Iziren <izirenjoel@gmail.com>
2024-09-06 02:24:37 +0100

    updated task

--- 0x01-react_intro/task_3/dashboard/src/App.test.js
+++ 0x01-react_intro/task_3/dashboard/src/App.test.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { shallow } from 'enzyme';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
@@ -6,4 +7,15 @@ test('renders login text', () => {
   render(<App />);
   const loginText = screen.getByText(/Login to access the full dashboard/i);
   expect(loginText).toBeInTheDocument();
+});
+
+describe('<App />', () => {
+  it('should render without crashing', () => {
+    shallow(<App />);
+  });
+
+  it('should contain the header text', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.find('h1').text()).toBe('School dashboard');
+  });
 });
--- 0x01-react_intro/task_5/dashboard/config/webpack.config.js
+++ 0x01-react_intro/task_5/dashboard/config/webpack.config.js
@@ -1,43 +1,54 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  entry: './dashboard/src/index.js',
+  entry: "./src/index.js",
   output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dashboard/dist'),
+    filename: "bundle.js",
   },
-  devServer: {
-    static: path.resolve(__dirname, '../dist'),
-    hot: true,
-    open: true,
-  },
+  mode: "development",
   module: {
     rules: [
       {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
       },
       {
-        test: /\.(png|jpg|jpeg|gif)$/i,
-        type: 'asset/resource',
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        // type: 'asset/resource',
         use: [
+          "file-loader",
           {
-            loader: 'image-webpack-loader',
+            loader: "image-webpack-loader",
             options: {
-              bypassOnDebug: true, // disable during development
-              disable: true, // disable optimization
+              bypassOnDebug: true, // webpack@1.x
+              disable: true, // webpack@2.x and newer
             },
           },
         ],
       },
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"],
+      },
     ],
   },
+  resolve: {
+    extensions: ["*", ".js", ".jsx"],
+  },
+  devServer: {
+    static: "./dist",
+    compress: true,
+    open: true,
+    hot: true,
+    port: 8564,
+  },
+  devtool: "inline-source-map",
   plugins: [
     new HtmlWebpackPlugin({
-      template: './dashboard/dist/index.html',
+      name: "index.html",
+      inject: false,
+      template: "./dist/index.html",
     }),
   ],
-  devtool: 'inline-source-map',
-  mode: 'development',
 };
