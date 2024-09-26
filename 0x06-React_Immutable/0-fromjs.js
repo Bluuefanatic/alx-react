540eb24a9139a148aa26471f8c52bf0420e03bd5
root <izirenjoel@gmail.com>
2024-09-26 21:40:11 +0000

    react immutable

--- 0x06-React_Immutable/0-fromjs.js
+++ 0x06-React_Immutable/0-fromjs.js
@@ -1,11 +1,5 @@
 const { fromJS } = require('immutable');
 
-/**
- * Converts a plain JavaScript object into an Immutable Map.
- * 
- * @param {Object} object - The object to convert to an Immutable Map.
- * @returns {Immutable.Map} - The Immutable Map created from the object.
- */
 function getImmutableObject(object) {
     return fromJS(object);
 }
